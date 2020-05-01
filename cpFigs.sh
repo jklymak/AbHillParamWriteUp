ee6d3ff8e0d027d85d844cb4106d266f3b78d989
Jody Klymak <jklymak@gmail.com>
2020-05-01 11:46:22 -0700

    First push

---
+++ cpFigs.sh
@@ -0,0 +1,1 @@
+cp /Users/jklymak/ForcedChannel5k/figs/ExampleTemps.pdf /Users/jklymak/ForcedChannel5k/figs/Spectra2D.pdf /Users/jklymak/ForcedChannel5k/figs/UTempCont.pdf /Users/jklymak/ForcedChannel5k/figs/BottomPressure.pdf /Users/jklymak/ForcedChannel5k/figs/TimeSeries305.pdf  /Users/jklymak/ForcedChannel5k/figs/VaryH.pdf /Users/jklymak/Dropbox/Goff_bathy/GoffResults.pdf figs
